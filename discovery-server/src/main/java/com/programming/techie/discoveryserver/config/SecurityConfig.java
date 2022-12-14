4a56efaf9a4f910953e2987f553be3e9a11438e9
Mohamad <yusha.9ine@gmail.com>
2022-12-14 13:36:59 +0330

    first changes

--- discovery-server/src/main/java/com/programming/techie/discoveryserver/config/SecurityConfig.java
+++ discovery-server/src/main/java/com/programming/techie/discoveryserver/config/SecurityConfig.java
@@ -1,34 +1,45 @@
 package com.programming.techie.discoveryserver.config;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
+@EnableWebSecurity
+public class SecurityConfig {
 
-    @Value("${app.eureka.username}")
+    @Value("${eureka.username}")
     private String username;
-    @Value("${app.eureka.password}")
+
+    @Value("${eureka.password}")
     private String password;
 
-    @Override
-    public void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.inMemoryAuthentication()
-                .passwordEncoder(NoOpPasswordEncoder.getInstance())
-                .withUser(username).password(password)
-                .authorities("USER");
+    @Bean
+    public InMemoryUserDetailsManager userDetailsService() {
+        UserDetails user = User.withDefaultPasswordEncoder()
+                .username(username)
+                .password(password)
+                .roles("USER")
+                .build();
+        return new InMemoryUserDetailsManager(user);
     }
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.csrf().disable()
-                .authorizeRequests().anyRequest()
-                .authenticated()
+                .authorizeRequests().anyRequest().authenticated()
                 .and()
                 .httpBasic();
+
+        return http.build();
     }
 }
